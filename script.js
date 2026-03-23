c8be0fc68a0f6fac0711c0a5beb5f003118cd431
elonyized <talktougwaibb@gmail.com>
2026-03-23 15:51:07 +0100

    Another modification

---
+++ script.js
@@ -0,0 +1,10 @@
+let form = document.querySelector("#form")
+let displayAlert = document.querySelector("#displayAlert")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    displayAlert.textContent = "You have submitted successfully";
+    displayAlert.className = "alert alert-success";
+
+})
+
